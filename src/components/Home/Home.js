e85520a778f62ebf2c27f13a53cf5c11e8d02338
SoySayli <114822629+SoySayli@users.noreply.github.com>
2023-10-17 08:20:12 +0200

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,6 +8,7 @@ import Type from "./Type";
 function Home() {
   return (
     <section>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5zTSZWYD9bIhrpeJbt42sp?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
       <Container fluid className="home-section" id="home">
         <Particle />
         <Container className="home-content">
