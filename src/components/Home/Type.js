2175d38abd2905ff2a8c6eff7ca2c4f2af351711
SoySayli <114822629+SoySayli@users.noreply.github.com>
2023-10-17 06:55:33 +0200

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,8 +8,8 @@ function Type() {
         strings: [
           "Software Developer",
           "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Designer",
+          "Graphic Artist",
         ],
         autoStart: true,
         loop: true,
