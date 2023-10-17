14e687db16229fc4a1126801fa343544a88113ac
SoySayli <114822629+SoySayli@users.noreply.github.com>
2023-10-17 08:38:32 +0200

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -106,6 +106,16 @@ function Home2() {
                 </a>
               </li>
             </ul>
+                     <iframe
+                style={{ borderRadius: "12px" }}
+                src="https://open.spotify.com/embed/track/5zTSZWYD9bIhrpeJbt42sp?utm_source=generator"
+                width="100%"
+                height="152"
+                frameBorder="0"
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+              ></iframe>
           </Col>
         </Row>
       </Container>
